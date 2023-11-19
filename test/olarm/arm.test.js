2bca031e7b449673e88678d6b0e29569762dbcae
Francois Buys <buys.fran@gmail.com>
2023-11-20 00:09:38 +0200

    --wip-- [skip ci]

--- test/olarm/arm.test.js
+++ test/olarm/arm.test.js
@@ -42,4 +42,37 @@ describe("olarm.arm", () => {
       },
     );
   });
+
+  describe("when arming and bypassing", () => {
+    it("works", async () => {
+      const n = await getNode("olarm1");
+      n.send = vi.fn();
+
+      n.receive({ payload: { "area-arm": "1", "zone-bypass": "2,3" } });
+      await Utils.delay(10);
+
+      expect(n.send).toHaveBeenCalledTimes(1);
+      expect(n.send.mock.calls[0][0]).toMatchObject({
+        areas: [{ label: "Area 01", state: "arm" }],
+        zones: [
+          { label: "Zone 01", state: "c" },
+          { label: "Zone 02", state: "b" },
+          { label: "Zone 03", state: "b" },
+          { label: "Zone 04", state: "c" },
+          { label: "Zone 05", state: "c" },
+          { label: "Zone 06", state: "c" },
+          { label: "Zone 07", state: "c" },
+          { label: "Zone 08", state: "c" },
+          { label: "Zone 09", state: "c" },
+          { label: "Zone 10", state: "c" },
+          { label: "Zone 11", state: "c" },
+          { label: "Zone 12", state: "c" },
+          { label: "Zone 13", state: "c" },
+          { label: "Zone 14", state: "c" },
+          { label: "Zone 15", state: "c" },
+          { label: "Zone 16", state: "c" },
+        ],
+      });
+    });
+  });
 });
